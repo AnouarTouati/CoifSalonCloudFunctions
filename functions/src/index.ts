df6b2f2087d6c29eda3dd91f220f6cad87acead7
Anouar Touati <41107778+KasperMA@users.noreply.github.com>
2020-08-08 15:14:18 +0100

    Works perfectly with Node8

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,32 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+//const requestModule = require('request')
 
-admin.initializeApp();
+admin.initializeApp()
 
- export const helloWorld = functions.https.onRequest((request, response) => {
-  admin.firestore().collection('Shops').doc("fff").collection("fefwe").doc("fewfew")
-  .set( {timeInMillis :admin.firestore.Timestamp.now().toMillis()},{merge:true})
-  .then(writeResult=>{
-        console.log("setting the time for pending was successful") 
-        response.send("Succesffully written")
-  }) .catch(error=>{
-    console.log(error) 
-    response.send("could not write"+error)
-  })
- });
+
+
+/*this works it outputs EAI_AGAIN  syscall getaddinfo
+EAI_AGAIN is a DNS lookup timed out erro
+meaning we are on free tier firebase we should upgrade to Blaze
+export const currenciesApi=functions.https.onRequest((request,response)=>{
+  const url="https://openexchangerates.org/api/latest.json?app_id=f43b3eae232d42a7b86dacab4395ef18"
+ requestModule(url,(error:any,res:any,body:any)=>{
+        console.log(res)
+        console.log(error)
+        console.log(body)
+       
+        if(!error){
+          response.send(res +" "+body)
+        }else{
+          response.send(error)
+        }
+ })
+
+})
+*/
+
+
 
  export const clientPendingTimeStamping=functions.firestore.document('/Shops/{ShopOwnerUid}/ClientsPending/{ClientUid}')
  .onCreate((snapshot,context)=>{
